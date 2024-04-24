84b5d2dcbe51df018c6a0bf694bad596fd7d3725
niysalomon <niysalomon@gmail.com>
2024-04-24 17:43:29 +0200

    adding card image slide

--- src/feature/houses/components/houseCard.tsx
+++ src/feature/houses/components/houseCard.tsx
@@ -1,17 +1,41 @@
 import Image from "next/image";
 import { HouseImage } from "../data/images";
 import { BathroomIcon, BedIcon, HomeIcon, LocationIcon } from "@/assets/icons";
+import Autoplay from "embla-carousel-autoplay";
+import useEmblaCarousel from "embla-carousel-react";
+import sliderData from "../data/sliderData";
 const HouseCard = ({
   setIsHouseDetails,
 }: {
   setIsHouseDetails: (_value: boolean) => void;
 }) => {
+  const [emblaRef] = useEmblaCarousel({ loop: false }, [Autoplay()])
   return (
     <>
       <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow p-1 text-gray-600 ">
-        <div>
+        {/* <div>
           <Image src={HouseImage} alt="" width={380} height={260} />
-        </div>
+        </div> */}
+        <div
+      className="overflow-hidden bg-gray-200  mx-auto flex items-center justify-center  "
+      ref={emblaRef}>
+      <div className="flex">
+        {sliderData?.map((item) => {
+          return (
+            <div className="embla__slide relative h-full w-full" key={item.id}>
+
+                {/* the image */}
+              <img className="w-full h-full rounded-t-lg" src={item.url} alt="" />
+
+              {/* title/subtitle */}
+              {/* <h1 className="absolute top-1/2 left-1/2 w-full md:w-auto transform -translate-x-1/2 translate-y-[3rem] md:translate-y-[9rem]  lg:translate-y-48 bg-cyan-600 py-2 lg:py-4 px-2 lg:px-8 text-xl lg:text-2xl text-white font-extrabold">
+                {item.title}
+              </h1> */}
+            </div>
+          );
+        })}
+      </div>
+    </div>
         <div className="p-5">
           <div className="flex items-center justify-between ">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import Layout from "@/common/layout";
 import RenterHouse from "@/feature/houses/renterHouse"; 
+import Slider from "@/feature/houses/slide";
 
 const Home=()=>{
     return(
         <>
         <Layout>
-        <RenterHouse/>
+        {/* <RenterHouse/> */}
+        <Slider/>
         </Layout>
         </>
     )
